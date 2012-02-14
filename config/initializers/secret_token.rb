5c7f48ea1a28fb7c5af15224bcb77974d6f322e5
Bruno Montanha <brunomontanha@Bruno-Montanhas-MacBook-Pro.local>
2012-02-14 17:23:05 -0200

    some files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flpdeaf::Application.config.secret_token = '1da56627ec0261bf305ac612e2b35456769842afa2c9e9f9b0ba1bedcd797471e850fceef5a030a131fa02c8b9245e335e412372dbdc7bdbde7439cdf3ed47cd'
